f4f1209819282243bc1612567d68c53a0461b71f
helllzy <115479353+helllzy@users.noreply.github.com>
2023-10-15 17:19:18 +0300

    Add files via upload

---
+++ StarkNet Bot/config.py
@@ -0,0 +1,156 @@
+######################################################
+# Main settings connected to account & transactions ##
+######################################################
+
+# True if you changed your accs private keys.
+# provide your addresses in data/adresses.txt
+CHANGED_KEY = False
+
+# all transactions will be completed via this RPC
+RPC = "https://starknet-mainnet.public.blastapi.io"
+
+# delay between accounts
+DELAY = [100, 200]
+
+# delay between modules
+MOD_DELAY = [25, 80]
+
+# all transactions will be completed below this gas price
+MAX_GWEI = 7.55
+
+# True if you want to achieve an exact nonce
+# if you chose this, modules will be calculated:
+# EXACT_NONCE_COUNT-your nonce and bot will choose modules from MODULES * 1000
+EXACT_NONCE = False
+
+# transactions count after script running
+EXACT_NONCE_COUNT = [70, 90]
+
+# True if you want to randomize moudules execution
+RANDOMIZE_MODULES = True
+
+# True if you want to randomize wallets execution
+RANDOMIZE_WALLETS = True
+
+# True if you want to use proxy. everyone account must have a proxy.
+# provide your proxies in data/proxies.txt
+USE_PROXY = False
+
+# True if you want to deploy wallets
+DEPLOY_WALLETS = False
+
+'''
+-----------------|available modules|-----------------|
+-- Don`t use "starkverse" cause project`s website    |
+-- doesn`t work, it looks like sybil activity        |
+-----------------------|swaps|-----------------------|
+"jedi_swap",                                         |
+"10k_swap",                                          |
+"myswap",                                            |
+"sith_swap",                                         |
+"avnu_swap",                                         |
+"starkex_swap",                                      |
+#"fibrous_swap" doesn`t work,mb you`ll find a mistake|
+----------------------|lending|----------------------|
+"zk_lending"                                         |
+---------------------|liquidity|---------------------|
+"jedi_liquidity",                                    |
+"10k_liquidity",                                     |
+"sith_liquidity"                                     |
+----------------|cheap transactions|-----------------|
+"collateral",                                        |
+"dmail",                                             |
+"starknet_id",                                       |
+"flex_market",                                       |
+"unframed_market",                                   |  
+"increase_limit"                                     |
+-----------------------------------------------------|
+'''
+
+# write there all modules from the table above you would like to complete
+MODULES = [
+    "collateral",
+    "dmail",
+    "starknet_id",
+    "flex_market",
+    "unframed_market",
+    "increase_limit"
+]
+
+# True if you want to skip some modules. it's good for account's randomization
+# if you chose this, modules will be calculated from MODULES with MODS_COUNT
+SKIP_MODS = True
+
+# how many modules do you need for each account?
+# each account will choose his own count
+MODS_COUNT = [3, 5]
+
+######################################################
+######## Settings below for swap or lending ##########
+######################################################
+
+# % of your balance for lending
+LENDING_PERCENTAGES = [50, 70]
+
+# True if you want to repeat zk_lending
+ZK_VOLUME = False
+
+# how many zk_lending repeats do you need
+ZK_REPEATS = [2, 3]
+
+# these tokens will be used for swap or liquidity
+ALLOWED_TOKENS = ['USDT', 'USDC', 'DAI', 'WBTC']
+
+# % of your balance for swap
+# swaps will be made first from ALLOWED_TOKENS in your wallet if you have any,
+# in another case bot will use your balance * SWAP_PERCENTAGES
+SWAP_PERCENTAGES = [1, 5]
+
+######################################################
+# Settings below connected to OKX withdraw & deposit #
+######################################################
+
+# True if you want to use your OKX to withdraw ETH to your accs.
+# provide your addresses in data/addresses.txt
+WITHDRAW_FROM_OKX = False
+
+# True if you want to transfer your ETH from subs to main OKX acc
+SUB_ACCS = False
+
+# how many ETH do you want to withdraw from OKX
+# each account will choose his own amount
+WITHDRAW_AMOUNT = [1.6, 2]
+
+# True if you want to transfer ETH from your accs to OKX.
+# provide your okx_addresses in data/okx_addresses.txt
+TRANSFER_TO_OKX = False
+
+# how many ETH do you want to keep in your wallet
+# after transfer to OKX
+AMOUNT_TO_KEEP = [0.011, 0.015]
+
+# OKX fee of ETH withdraw to StarkNet
+FEE = 0.0001
+
+# https://www.okx.com/account/my-api 
+# there you can take your API keys. It needs only for OKX withdraw
+OKX_KEYS = {
+    'api_key'   : '',
+    'api_secret': '',
+    'password'  : ''
+}
+
+######################################################
+########### Settings below for TG messages ###########
+######################################################
+
+# https://t.me/BotFather
+# there you can take API key for your TG bot
+BOT_TOKEN = ''
+
+# https://t.me/getmy_idbot
+# there you can take your TG ID
+CHAT_ID = 0
+
+# ETH price needs to send you calculated FEE for each account
+CURRENT_ETH_PRICE = 1570
---
+++ StarkNet Bot/modules/utils.py
@@ -0,0 +1,192 @@
+from decimal import Decimal, ROUND_HALF_UP
+from random import randint, choice
+from functools import wraps
+from asyncio import sleep
+from requests import post
+from loguru import logger
+from web3 import Web3
+
+from starknet_py.net.gateway_client import GatewayClient
+from starknet_py.contract import Contract
+
+from data.data import (
+    EMOJI_NUMBERS,
+    TOKENS_ABI,
+    USDT_ADDRESS,
+    USDC_ADDRESS,
+    DAI_ADDRESS,
+    ZKLEND_ETH_ADDRESS,
+    WBTC_ADDRESS
+)
+from config import (
+    MAX_GWEI,
+    ALLOWED_TOKENS,
+    BOT_TOKEN, CHAT_ID,
+    CURRENT_ETH_PRICE
+)
+
+logger.add('data/logs.log')
+
+
+async def result_data(account, balance) -> int:
+    while True:
+
+        try:
+           return (balance - (await account.get_balance())) / 1e18
+
+        except:
+            await sleep(5)
+
+
+async def approve(contract_address, account, spender, amount):
+    contract = Contract(
+            address=contract_address,
+            abi=TOKENS_ABI,
+            provider=account
+        )
+
+    approve = contract.functions['approve'].prepare(
+            spender=spender,
+            amount=amount
+        )
+
+    return approve
+
+
+async def check_balance(account) -> str | int | None:
+    while True:
+        try:
+            for token in [None, USDT_ADDRESS, USDC_ADDRESS, \
+                          DAI_ADDRESS, ZKLEND_ETH_ADDRESS, WBTC_ADDRESS]:
+
+                balance = await account.get_balance(token)
+
+                if not token and balance/1e14 > 5: # 0.8$
+                    return balance
+
+                elif not token and balance/1e14 < 5: # 0.8$
+                    await send_message(f"{hex(account.address)} has insufficient balance:"
+                                       f"{round(balance/1e18, 5)}")
+                    logger.error(f"{hex(account.address)} has insufficient balance "
+                                 f"{round(balance/1e18, 5)}")
+                    return
+
+                elif token == ZKLEND_ETH_ADDRESS and balance/1e14 > 3: # 0.5$
+                    return "zklend_supply"
+
+                elif token == WBTC_ADDRESS and balance/1e2 > 12 or token and balance/1e5 > 5: # 0.5$
+                    return choice(["jedi_swap", "10k_swap", "sith_swap"])
+
+                await sleep(3)
+        except:
+            logger.error('Got an error while checking balance, trying again in 10 secs')
+            await sleep(10)
+
+
+async def send_message(text) -> None:
+    if isinstance(text, dict):
+        message = ''
+        for addr in list(text.keys()):
+            message += f'{add_emoji(text[addr]["id"])} | {addr[:4]}...{addr[-4:]}' \
+                       f'| FEE: {round(text[addr]["fee"]*CURRENT_ETH_PRICE, 2)}$' \
+                       f'| {"; ".join(text[addr]["mods"])}\n'
+
+        text = message.strip()
+    post(
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id="
+        f"{CHAT_ID}&text={text}"
+        )
+
+
+def add_emoji(text) -> str:
+    return ''.join([EMOJI_NUMBERS[num] for num in text])
+
+
+async def exec(self, module, calls) -> None:
+    module = module.capitalize()
+
+    tx_response = await self.account.execute(calls=calls, auto_estimate=True)
+    info(
+        f"| {module} | Waiting transaction: "
+        f"https://starkscan.co/tx/{hex(tx_response.transaction_hash)}", "magenta"
+        )
+
+    await self.account.client.wait_for_tx(tx_response.transaction_hash)
+    logger.success(f"| {module} | Transaction accepted")
+
+
+async def sleeping(secs, text=None, color=None) -> None:
+    if text:
+        info(text, color)
+
+    await sleep(randint(*secs))
+
+
+def info(text, color="white") -> None:
+    logger.opt(colors=True).info(f'<{color}>{text}</{color}>')
+
+
+def check_gas(func):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        gateway_client = GatewayClient(net="mainnet")
+
+        while True:
+            try:
+                w3 = Web3(Web3.HTTPProvider(choice(
+                    ['https://rpc.ankr.com/eth', 'https://1rpc.io/eth'])))
+                block = await gateway_client.get_block(block_number='pending')
+                stark_gas_price = round(block.gas_price / 10 ** 9, 2)
+                eth_gas_price = round(w3.from_wei(w3.eth.gas_price, 'gwei'), 2)
+
+                if stark_gas_price > MAX_GWEI or eth_gas_price > MAX_GWEI:
+                    logger.warning(
+                        f"STARK gas`s {stark_gas_price} | "
+                        f"ETH gas`s {eth_gas_price} | sleep 30 seconds"
+                        )
+                    await sleep(30)
+                else:
+                    break
+
+            except:
+                pass
+
+        return await func(*args, **kwargs)
+    return wrapper
+
+
+def use_slippage(v, slippage) -> int:
+        slippage_values = {
+            0.1: lambda x: (x / 1000) * 999,
+            0.5: lambda x: (x / 1000) * 995,
+            1: lambda x: (x / 1000) * 990,
+            2: lambda x: (x / 1000) * 980,
+        }
+
+        v = Decimal(v)
+        slippage_fn = slippage_values.get(slippage, lambda x: x)
+
+        result = slippage_fn(v).quantize(Decimal('1'), rounding=ROUND_HALF_UP)
+
+        return int(result)
+
+
+def prepare_token() -> int:
+    match choice(ALLOWED_TOKENS):
+
+        case 'USDT':
+            token = USDT_ADDRESS
+
+        case 'USDC':
+            token = USDC_ADDRESS
+
+        case 'DAI':
+            token = DAI_ADDRESS
+
+        case 'WBTC':
+            token = WBTC_ADDRESS
+
+        case _:
+            logger.critical("WRONG TOKEN, CHECK ALLOWED TOKENS!")
+
+    return token
